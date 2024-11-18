a37aefe422c7b952cf2fbda382d4528fcc8bbfca
1337m3rcy <memeslam1337@bk.ru>
2024-11-19 01:24:34 +0300

    Add panels and buttons with icons

---
+++ src/Dex.AnomalyCustom.Launcher.Avalonia/Views/SettingsView.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Dex.AnomalyCustom.Launcher.Avalonia;
+
+public partial class SettingsView : UserControl
+{
+    public SettingsView() => InitializeComponent();
+}

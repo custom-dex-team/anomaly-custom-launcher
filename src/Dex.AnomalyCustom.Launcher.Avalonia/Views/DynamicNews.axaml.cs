1864e870a8d4317e20664ff9fd0919cdf66d89e5
1337m3rcy <memeslam1337@bk.ru>
2024-11-21 14:28:07 +0400

    Add empty view for dynamic news

---
+++ src/Dex.AnomalyCustom.Launcher.Avalonia/Views/DynamicNews.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Dex.AnomalyCustom.Launcher.Avalonia;
+
+public partial class DynamicNews : UserControl
+{
+    public DynamicNews()
+    {
+        InitializeComponent();
+    }
+}

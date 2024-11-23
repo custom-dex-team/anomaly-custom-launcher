ca3ba3f4a6148d372efd015a7394a4add2998ddd
acidicMercury8 <acidicmercury8@outlook.com>
2024-11-23 15:54:49 +0300

    Add commands to main view

--- src/Dex.AnomalyCustom.Launcher.Avalonia/ViewModels/MainViewModel.cs
+++ src/Dex.AnomalyCustom.Launcher.Avalonia/ViewModels/MainViewModel.cs
@@ -1,8 +1,27 @@
 using ReactiveUI;
 
+using System.Windows.Input;
+
 namespace Dex.AnomalyCustom.Launcher.Avalonia.ViewModels;
 
 public class MainViewModel : ReactiveObject
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public ICommand OpenVKCommand { get; }
+
+    public ICommand PlayCustomCommand { get; }
+
+    public MainViewModel()
+    {
+        OpenVKCommand = ReactiveCommand.Create(OpenVK);
+
+        PlayCustomCommand = ReactiveCommand.Create(PlayCustom);
+    }
+
+    private void OpenVK()
+    {
+    }
+
+    private void PlayCustom()
+    {
+    }
 }
